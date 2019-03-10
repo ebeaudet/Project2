7d8ec2c8f06c9b15ecfe0a0f3cc2e6b1e9da6c6f
Lily <44213621+lilysimmons@users.noreply.github.com>
2019-03-10 19:45:53 -0400

    api dom connection successful

--- public/assets/js/triviaClick.js
+++ public/assets/js/triviaClick.js
@@ -1,53 +1,85 @@
 
-// $(document).ready(function () {
-    var triviaCategories = {
-        9: "General Knowledge",
-        10: "Entertainment: Books",
-        11: "Entertainment: Film",
-        12: "Entertainment: Music",
-        14: "Entertainment: Television",
-        15: "Entertainment: Video Games",
-        17: "Science & Nature",
-        18: "Science: Computers",
-        19: "Science: Mathematics",
-        20: "Mythology",
-        21: "Sports",
-        22: "Geography",
-        23: "History",
-        24: "Politics",
-        25: "Art"
-    };
+// // $(document).ready(function () {
+//     var triviaCategories = {
+//         9: "General Knowledge",
+//         10: "Entertainment: Books",
+//         11: "Entertainment: Film",
+//         12: "Entertainment: Music",
+//         14: "Entertainment: Television",
+//         15: "Entertainment: Video Games",
+//         17: "Science & Nature",
+//         18: "Science: Computers",
+//         19: "Science: Mathematics",
+//         20: "Mythology",
+//         21: "Sports",
+//         22: "Geography",
+//         23: "History",
+//         24: "Politics",
+//         25: "Art"
+//     };
 
-    var randomCategoryArray = [];
-    function randomCategory() {
-        randomCat = Math.floor(Math.random() * triviaCategories.length + 1);
-        // console.log(randomCat);
-        if (randomCategoryArray.indexOf(randomCat) > -1) {
-            randomCategory();
-        }
-    }
-
-
-
-
-    function populateCats() {
-        randomCategoryArray = [];
-        randomCategory();
-        randomCategoryArray.push(randomCat);
-        randomCategory();
-        randomCategoryArray.push(randomCat);
-        randomCategory();
-        randomCategoryArray.push(randomCat);
-        randomCategory();
-        randomCategoryArray.push(randomCat);
-        console.log(randomCategoryArray);
-    }
+//     var randomCategoryArray = [];
+//     function randomCategory() {
+//         randomCat = Math.floor(Math.random() * triviaCategories.length + 1);
+//         // console.log(randomCat);
+//         if (randomCategoryArray.indexOf(randomCat) > -1) {
+//             randomCategory();
+//         }
+//     }
 
 
 
-    populateCats();
+
+//     function populateCats() {
+//         randomCategoryArray = [];
+//         randomCategory();
+//         randomCategoryArray.push(randomCat);
+//         randomCategory();
+//         randomCategoryArray.push(randomCat);
+//         randomCategory();
+//         randomCategoryArray.push(randomCat);
+//         randomCategory();
+//         randomCategoryArray.push(randomCat);
+//         console.log(randomCategoryArray);
+//     }
+
+
+
+//     populateCats();
+
+
+//And their corresponding codes for API calls
+// var triviaCodes = [9, 10, 11, 12, 14, 15, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+// // var randomCategoryArray = [];
+// var randomCategoryCodes = [];
+
+// //Random category picks randomly from trivia array. If category already exists in the random category array already it runs again.
+// function randomCategory() {
+//   randomCat = Math.floor(Math.random() * triviaCodes.length + 1);
+//   if (randomCategoryArray.indexOf(triviaCodes[randomCat - 1]) > -1) {
+//     randomCategory();
+//   }
+// }
+// // populate cats empties the randomCategoryArray, then picks three random categories and pushes them to the random category array. the if statement in the previously defined randomCategory function makes sure that they are unique. Every time we run populateCats, three random, unique intergers from 1 to the length of the triviaCategories are array are generated and pushed to the randomCategoryArray.
+
+// function populateCats() {
+//   // randomCategoryArray = [];
+//   randomCategoryCodes = [];
+//   var i;
+//   for (i = 0; i < 4; i++) {
+//     randomCategory();
+//     // randomCategoryArray.push(triviaCategories[randomCat - 1]);
+//     randomCategoryCodes.push(triviaCodes[randomCat - 1]);
+//   }
+//   // console.log(randomCategoryArray);
+//   console.log(randomCategoryCodes);
+  
 
 // });
+
+// $(document).on("click", ".catColOne", function (e) {
+
+// var 
 
 
 // $(function () {
--- public/assets/js/triviaquery.js
+++ public/assets/js/triviaquery.js
@@ -1,88 +1,102 @@
-$(document).ready(function() {
-  //Trivia categories from the API as strings
-  var triviaCategories = [
-    "General Knowledge",
-    "Entertainment: Books",
-    "Entertainment: Film",
-    "Entertainment: Music",
-    "Entertainment: Television",
-    "Entertainment: Video Games",
-    "Science & Nature",
-    "Science: Computers",
-    "Science: Mathematics",
-    "Mythology",
-    "Sports",
-    "Geography",
-    "History",
-    "Politics",
-    "Art"
-  ];
-  //And their corresponding codes for API calls
-  var triviaCodes = [9, 10, 11, 12, 14, 15, 17, 18, 19, 20, 21, 22, 23, 24, 25];
-  var randomCategoryArray = [];
-  var randomCategoryCodes = [];
+$(document).ready(function () {
 
-  //Random category picks randomly from trivia array. If category already exists in the random category array already it runs again.
-  function randomCategory() {
-    randomCat = Math.floor(Math.random() * triviaCategories.length + 1);
-    if (randomCategoryArray.indexOf(triviaCategories[randomCat - 1]) > -1) {
-      randomCategory();
-    }
-  }
-  // populate cats empties the randomCategoryArray, then picks three random categories and pushes them to the random category array. the if statement in the previously defined randomCategory function makes sure that they are unique. Every time we run populateCats, three random, unique intergers from 1 to the length of the triviaCategories are array are generated and pushed to the randomCategoryArray.
+  // var category = 12;
+  // var difficulty = "easy";
+  var queryURL = "https://opentdb.com/api.php?amount=10";
+  var catQueryUrl = "https://opentdb.com/api_category.php"
+  // var queryCat = "&category=" + category;
+  // var queryDiff = "&difficulty=" + difficulty;
+  // var multi = "&type=multiple"
 
-  function populateCats() {
-    randomCategoryArray = [];
-    randomCategoryCodes = [];
-    var i;
-    for (i = 0; i < 4; i++) {
-      randomCategory();
-      randomCategoryArray.push(triviaCategories[randomCat - 1]);
-      randomCategoryCodes.push(triviaCodes[randomCat - 1]);
-    }
-    console.log(randomCategoryArray);
-    console.log(randomCategoryCodes);
-    $(".catOne").append(randomCategoryArray[0]);
-    $(".catTwo").append(randomCategoryArray[1]);
-    $(".catThree").append(randomCategoryArray[2]);
-    $(".catFour").append(randomCategoryArray[3]);
-    $("#c1q1").addClass("&category=" + randomCategoryCodes[0]);
-    $("#c1q1", "c1q2", "c1q3").data(
-      "category",
-      "&category=" + randomCategoryCodes[0]
-    );
-    $("#c2q1", "c2q2", "c2q3").data(
-      "category",
-      "&category=" + randomCategoryCodes[1]
-    );
-    $("#c3q1", "c3q2", "c3q3").data(
-      "category",
-      "&category=" + randomCategoryCodes[2]
-    );
-    $("#c1q4", "c1q4", "c1q4").data(
-      "category",
-      "&category=" + randomCategoryCodes[3]
-    );
-    console.log("c1q4 data right here" + $("#c1q4").data());
-  }
+  $.ajax({
+    url: catQueryUrl,
+    method: "GET"
+  })
+    // After data comes back from the request
+    .then(function (response) {
+      console.log(catQueryUrl);
 
-  populateCats();
+      console.log(response);
+      // storing the data from the AJAX request in the results variable
+      var results = response.trivia_categories;
+      console.log(results);
+      var catbox = [];
 
-  // temporary defaults for category and difficulty
-  var category = 12;
-  var difficulty = "easy";
-  var queryStart = "https://opentdb.com/api.php?amount=1&type=multiple";
-  var queryCat = "&category=" + category;
-  var queryDiff = "&difficulty=" + difficulty;
 
-  // queryURL puts the two halves together
-  var queryUrl = queryStart + queryCat + queryDiff;
+      // if (randomCat.indexOf(catbox[randomCat - 1]) > -1) {
+      //       randomCategory();
+      // Looping through each result item
+      for (var i = 0; i < 4; i++) {
 
-  function getQuestion(queryCat, queryDiff) {
-    $.get(queryStart + queryCat + queryDiff, function(data) {
-      console.log("Posts", data);
-    });
-  }
-  getQuestion(queryCat, queryDiff);
-  getQuestion("&category=" + 14, "&difficulty=hard");
-});
+        var randomCat = Math.floor(Math.random() * results.length);
+        console.log("random cat " + randomCat);
+
+        var catId = results[i].id
+        console.log("cat Id " + catId);
+
+        var catName = results[randomCat].name;
+        catbox.push(catName);
+        console.log("cat name " + catName)
+
+      }
+
+      $("#catOne").append(catbox[0]);
+      $("#catTwo").append(catbox[1]);
+      $("#catThree").append(catbox[2]);
+      $("#catFour").append(catbox[3]);
+    })
+
+
+
+})
+
+
+
+
+// $(".catColOne").addClass("&category="+ catbox[0]);
+// $(".catColTwo").addClass("&category="+ catbox[1]);
+// $(".catColThree").addClass("&category="+ catbox[2]);
+// $(".catColFour").addClass("&category="+ catbox[3]);
+
+
+      //     $("#c1q1").addClass("&category=" + randomCategoryCodes[0]);
+//     $("#c1q1", "c1q2", "c1q3").data(
+//       "category",
+//       "&category=" + randomCategoryCodes[0]
+//     );
+//     $("#c2q1", "c2q2", "c2q3").data(
+//       "category",
+//       "&category=" + randomCategoryCodes[1]
+//     );
+//     $("#c3q1", "c3q2", "c3q3").data(
+//       "category",
+//       "&category=" + randomCategoryCodes[2]
+//     );
+//     $("#c1q4", "c1q4", "c1q4").data(
+//       "category",
+//       "&category=" + randomCategoryCodes[3]
+//     );
+//     console.log("c1q4 data right here" + $("#c1q4").data());
+//   }
+
+//   populateCats();
+
+//   // temporary defaults for category and difficulty
+//   var category = 12;
+//   var difficulty = "easy";
+//   var queryStart = "https://opentdb.com/api.php?amount=1&type=multiple";
+//   var queryCat = "&category=" + category;
+//   var queryDiff = "&difficulty=" + difficulty;
+
+//   // queryURL puts the two halves together
+//   var queryUrl = queryStart + queryCat + queryDiff;
+//   console.log(queryUrl);
+
+//   function getQuestion(queryCat, queryDiff) {
+//     $.get(queryStart + queryCat + queryDiff, function(data) {
+//       console.log("Posts", data);
+//     });
+//   }
+//   getQuestion(queryCat, queryDiff);
+//   getQuestion("&category=" + 14, "&difficulty=hard");
+// });
