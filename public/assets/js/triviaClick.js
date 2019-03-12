d2f288f7abfabcb6ec60f6371d456dde602396f9
unknown <ed.beaudet@gmail.com>
2019-03-12 16:53:15 -0400

    answers were not being randomized. Fixed

--- public/assets/js/triviaClick.js
+++ public/assets/js/triviaClick.js
@@ -7,31 +7,16 @@ function pullQuestionsAPI() {
   })
     // After data comes back from the request
     .then(function(response) {
-      console.log(queryURL);
-
-      // console.log(response);
-
       var results = response.results;
-      console.log(results);
 
       for (var i = 0; i < results.length; i++) {
-        // console.log(results.length)
         var category = results[i].category;
-        console.log(category);
         var question = results[i].question;
-        console.log(question);
-
         var correct = results[i].correct_answer;
-        console.log(correct);
-
         var incorrect = results[i].incorrect_answers;
-        // var incorrectArr = []
-        console.log(incorrect);
-
         var allAnswers = [];
         var questionObj = {};
-        // incorrectArr.push(incorrect)
-        // console.log(incorrectArr); 
+
         questionObj.incorrect = incorrect;
         questionObj.correct = correct;
         questionObj.question = question;
@@ -49,12 +34,12 @@ function pullQuestionsAPI() {
       $(".questionDiv").append(question);
 
       for (var i = 0; i < allAnswers.length; i++) {
-        console.log("these are all " + allAnswers);
+        console.log("these are all " + allAnswers);i
         var answersDiv = $("<div>");
-        var answer = Math.floor(Math.random() * allAnswers.length);
+        var answer = Math.floor(Math.random() * (allAnswers.length - 1));
         var button = $("<button>");
-        button.text(allAnswers[i]);
-        console.log(answer[i]);
+        button.text(allAnswers[answer]);
+        console.log(allAnswers[answer]);
         answersDiv.append(button);
         $(".answers").append(answersDiv);
         button.addClass("btnAnswer btn-primary");
