eb559f3be4d6086ec7aa65067947c333b30c8911
SamHanson <samueldavidhanson@gmail.com>
2019-03-12 22:53:16 -0400

    updating players correctly and player selection screen

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,9 +1,19 @@
-console.log("index file loaded");
-
 // Page elements
 var $newPlayerName = $("#newPlayerName");
 var $newPlayerButton = $("#newPlayerButton");
+
 var $highscores = $("#highScores");
+var $numPlayersButtons = $(".numPlayersButton");
+var $numPlayersHeader = $("#numPlayersHeader");
+var $newPlayerArea = $("#newPlayerArea");
+var $currentPlayersArea = $("#currentPlayersArea");
+
+var $startGame = $("#startGame");
+
+// Variables
+var numPlayers = 0;
+var chosenPlayers = 0;
+var currentPlayers = [];
 
 // The API object contains methods for each kind of request we'll make
 var API = {
@@ -31,6 +41,12 @@ var API = {
       type: "GET"
     });
   },
+  getPlayer: function(id) {
+    return $.ajax({
+      url: "api/players/"+id,
+      type: "GET"
+    });
+  },
   deletePlayer: function(id) {
     return $.ajax({
       url: "api/players/" + id,
@@ -43,19 +59,18 @@ API.getPlayers();
 
 // refreshPlayers gets new examples from the db and repopulates the list
 var refreshPlayers = function() {
-  console.log("getting players");
   API.getPlayers().then(function(data) {
-    console.log(data);
     var $players = data.map(function(player) {
-      console.log(player);
-      var $row = $("<div>");
+      var $row = $("<span style='display: inline-block; padding: 5px; background: lightblue; border: 1px solid black'>");
       $row.addClass("player");
+      $row.attr("id","player"+player.id);
 
       $row.append("<p>" + player.playerName + " </p>");
       $row.append("<p>Score: " + player.score + "</p>");
       $row.append("<p>Wins: " + player.wins + "</p>");
       $row.append("<p>Losses: " + player.losses + "</p>");
-      $row.append("<button onclick='handleDeleteBtnClick("+player.id+")'>x</button>");
+      $row.append("<button style='background-color: lightgreen' onclick='playerChosen("+player.id+")'>Select</button>");
+      $row.append("<button style='background-color: darkred' onclick='handleDeleteBtnClick("+player.id+")'>Remove</button>");
 
       return $row;
     });
@@ -73,7 +88,10 @@ var playerFormSubmit = function(event) {
 
   var newPlayer = {
     playerName: $newPlayerName.val().trim(),
-    round: 0
+    score: 0,
+    round: 0,
+    wins: 0,
+    losses: 0
   }
 
   if (!newPlayer.playerName) {
@@ -81,13 +99,9 @@ var playerFormSubmit = function(event) {
     return;
   }
 
-  console.log(newPlayer);
   API.savePlayer(newPlayer).then(function() {
     refreshPlayers();
   });
-  // API.saveThis(newPlayer).then(function() {
-  //   refreshPlayers();
-  // });
 
   $newPlayerName.val("");
 };
@@ -100,6 +114,55 @@ var handleDeleteBtnClick = function(id) {
     refreshPlayers();
   });
 };
+
+var numPlayersChosen = function(quantity) {
+  console.log("Number of players chosen!");
+  console.log(quantity);
+  numPlayers = quantity;
+  //$highscores.style.display = 'block';
+  //$newPlayerButton.display = 'none';
+  $highscores.css("display", "block");
+  $newPlayerArea.css("display", "block");
+  $numPlayersButtons.css("display", "none");
+  $numPlayersHeader.css("display", "none");
+}
+
+var playerChosen = function(playerID) {
+  $("#player"+playerID).css("display", "none");
+  chosenPlayers++;
+
+  API.getPlayer(playerID).then(function(player) {
+    currentPlayers.push(player);
+  });
+
+  if (chosenPlayers >= numPlayers) {
+    console.log("All players selected");
+    $highscores.css("display", "none");
+    $startGame.css("display", "block");
+    $newPlayerArea.css("display", "none");
+
+    for (var i = 1; i <= currentPlayers.length; i++) {
+      var $row = $("<span style='display: inline-block; padding: 5px; background: lightblue; border: 1px solid black'>");
+      $row.append("<p style='font-weight: bold;'>Player "+i+"</p>");
+      $row.append("<p>" + currentPlayers[i-1].playerName + " </p>");
+      $row.append("<p>Score: " + currentPlayers[i-1].score + "</p>");
+      $row.append("<p><span style='padding: 2px';>Wins: " + currentPlayers[i-1].wins + "</span>"+
+        "<span style='padding:2px;'>Losses: " + currentPlayers[i-1].losses +"</span></p>");
+      $currentPlayersArea.append($row);
+      $currentPlayersArea.append("<span style='font-weight: bold; font-size: 20px; padding: 3px'>VS</span>");
+    }
+    API.getPlayer(playerID).then(function(player) {
+      var $row = $("<span style='display: inline-block; padding: 5px; background: lightblue; border: 1px solid black'>");
+      $row.append("<p style='font-weight: bold;'>Player "+chosenPlayers+"</p>");
+      $row.append("<p>" + player.playerName + " </p>");
+      $row.append("<p>Score: " + player.score + "</p>");
+      $row.append("<p><span style='padding: 2px';>Wins: " + player.wins + "</span>"+
+        "<span style='padding:2px;'>Losses: " + player.losses +"</span></p>");
+      $currentPlayersArea.append($row);
+    });
+  }
+  console.log(currentPlayers);
+}
 
 // Add event listeners to the submit and delete buttons
 $newPlayerButton.on("click", playerFormSubmit);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -42,12 +42,31 @@ module.exports = function(app) {
   });
 
   app.put("/api/players", function(req, res) {
-    // console.log("Req:");
-    // console.log(req);
-    // db.Players.create(req.body).then(function(dbPlayer) {
-    //   res.json(dbPlayer);
-    // });
+    db.Players.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbPlayer) {
+      res.json(dbPlayer);
+    });
   });
+
+
+  app.put("/api/posts", function(req, res) {
+    db.Post.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+
 
   // Delete a player by id
   app.delete("/api/players/:id", function(req, res) {
